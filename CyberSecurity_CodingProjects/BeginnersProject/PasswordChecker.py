0855ad8c7d0aad33b91e9561814ad78388b62872
Worldofmarvis <vistoedmar25@gmail.com>
2026-01-06 07:50:59 -0500

    Delete CyberSecurity_CodingProjects/BeginnersProject/PasswordChecker.py

--- CyberSecurity_CodingProjects/BeginnersProject/PasswordChecker.py
+++
@@ -1,8 +0,0 @@
-right_password = "Alpha123"
-password = input("Enter your password: ")
-if(password!=right_password):
-    for i in range(1,4):
-        print("Try Again!")
-        password = input("Enter your password: ")
-else:
-    print("You entered the right password.")
